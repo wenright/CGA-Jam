be95a262c72902780c75e8a3a3ed3b4e10bb96eb
wenright <wenright@purdue.edu>
2017-06-08 15:11:09 -0500

    Add AI shooting

--- Assets/Scripts/AiMovement.cs
+++ Assets/Scripts/AiMovement.cs
@@ -7,27 +7,45 @@ public class AiMovement : MonoBehaviour {
 	public Transform target;
 
 	private float speed = 750.0f;
-	private float rotationSpeed = 50.0f;
+	private float rotationSpeed = 100.0f;
+
+	private int breakAwayDistance = 100;
+	private Vector3 breakAwayOffset;
 
 	private Rigidbody rb;
+	private WeaponController weaponController;
 
 	void Start () {
 		rb = GetComponent<Rigidbody>();
+		weaponController = GetComponent<WeaponController>();
 	}
 	
 	void Update () {
 		if (target != null) {
 			float distance = Vector3.Distance(target.position, transform.position);
 
-			if (distance < 100) {
+			if (distance < breakAwayDistance) {
+				// If this is the first frame breaking away, set a random offset
+				if (breakAwayDistance != 250) {
+					breakAwayOffset = new Vector3(Random.Range(-45, 45), Random.Range(-45, 45), Random.Range(-45, 45));
+				}
+
+				breakAwayDistance = 250;
+
 				BreakAway();
 			} else {
+				breakAwayDistance = 100;
+
 				RotateTowardsTarget();
 
 				Vector3 targetVector = target.position - transform.position;
 				float angleBetween = Vector3.Angle(transform.forward, targetVector);
 				if (angleBetween <= 20) {
 					MoveTowardsTarget();
+				}
+
+				if (angleBetween <= 10) {
+					weaponController.Fire();
 				}
 			}
 		}
@@ -50,7 +68,7 @@ public class AiMovement : MonoBehaviour {
 
 	// Move away form the target as fast as possible, gaining gorund while preparing to turn to engage
 	private void BreakAway () {
-		Vector3 targetAngle = -Quaternion.LookRotation(target.position - transform.position).eulerAngles + new Vector3(Random.value * 90, Random.value * 90, Random.value * 90);
+		Vector3 targetAngle = -Quaternion.LookRotation(target.position - transform.position).eulerAngles + breakAwayOffset;
 		Vector3 currentAngle = transform.rotation.eulerAngles;
 		Vector3 torque = new Vector3(
 			Mathf.DeltaAngle(currentAngle.x, targetAngle.x),
--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -2,16 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerShooting : MonoBehaviour {
+public class WeaponController : MonoBehaviour {
 
 	public GameObject laser;
 	public Transform barrel;
+
+	public bool isPlayer = false;
 
 	private bool canFire = true;
 	private float ROF = 0.15f;
 
 	void Update () {
-		if (Input.GetButton("Fire1") && canFire) {
+		if (isPlayer) {
+			if (Input.GetButton("Fire1")) {
+				Fire();
+			}
+		}
+	}
+
+	public void Fire () {
+		if (canFire) {
 			Instantiate(laser, barrel.position, transform.rotation * Quaternion.Euler(0, 90, 90));
 
 			canFire = false;
