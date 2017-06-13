1285aaf33bb98d7c3f7f7fbfc85db4f0ff5b15ac
wenright <wenright@purdue.edu>
2017-06-12 20:02:03 -0500

    Add FTL intro effect

---
+++ Assets/Scripts/IntroFTLEffect.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class IntroFTLEffect : MonoBehaviour {
+
+	public CameraShake cameraShake;
+
+	void Start () {
+		float duration = 5.0f;
+
+		Camera.main.DOFieldOfView(43.4f, duration)
+			.SetEase(Ease.OutQuad);
+
+		cameraShake.Shake();
+	}
+	
+	void Update () {
+		
+	}
+}
