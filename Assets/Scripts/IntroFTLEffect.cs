d0009e31f652e0b55508da58b69ab70e3153e137
wenright <wenright@purdue.edu>
2017-06-12 20:07:01 -0500

    Tweak FTL intro effects and duration

--- Assets/Scripts/IntroFTLEffect.cs
+++ Assets/Scripts/IntroFTLEffect.cs
@@ -6,10 +6,11 @@ public class IntroFTLEffect : MonoBehaviour {
 	public CameraShake cameraShake;
 
 	void Start () {
-		float duration = 5.0f;
+		float duration = 2.0f;
 
-		Camera.main.DOFieldOfView(43.4f, duration)
-			.SetEase(Ease.OutQuad);
+		Camera.main.DOFieldOfView(120.0f, duration)
+			.SetEase(Ease.OutExpo)
+			.SetLoops(2, LoopType.Yoyo);
 
 		cameraShake.Shake();
 	}
